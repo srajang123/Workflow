05e2df7f42918f6c2588ad7b9836692f9f340bc3
Mann Mehta <17it051@charusat.edu.in>
2020-08-18 01:18:56 +0530

    Added HomeComponent

---
+++ frontend/src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className = "container">
+            <div className= "row">
+                <h1>Home Page</h1>
+            </div>
+        </div>
+    );
+}
+export default Home;
