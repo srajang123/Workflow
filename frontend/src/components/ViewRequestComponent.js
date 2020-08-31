12cb19b68a4dea6b72f06f1558d9a8de78a91aad
Mann Mehta <17it051@charusat.edu.in>
2020-08-31 15:27:03 +0530

    Approver Active/All request frontend with modal complete

--- frontend/src/components/ViewRequestComponent.js
+++ frontend/src/components/ViewRequestComponent.js
@@ -1,13 +1,50 @@
 import React, { Component, Fragment } from 'react';
-import { Spinner, Row, Col, Card, Button, CardHeader, CardBody, CardText ,ListGroup, ListGroupItem} from 'reactstrap';
-
-// It will receive data in props for which request to show
+import { Spinner, Row, Col, Card, Button, CardHeader, 
+    CardBody, CardText ,ListGroup, ListGroupItem,
+    Modal, ModalHeader, ModalBody, ModalFooter, Input, Label} from 'reactstrap';
 
 class SingleCardBody extends Component{
     constructor(props) {
         super(props);
-        this.review = this.review.bind(this);
+        this.state = {
+            modal : false,
+            approverNote : "",
+            touched : {
+                approverNote : false,
+            }
+        }
+        this.toggle = this.toggle.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
     }
+
+    toggle() {
+        console.log(this.state.touched);
+        this.setState({
+            modal : !this.state.modal
+        })
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name] : value,
+       });
+    }
+
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched : { ...this.state.touched, [field] : true }
+        })
+    }
+
+
+    handleSubmit = async (event) => {
+    }
+
 
     review(data) {
         // trigger modal here
@@ -21,18 +58,34 @@ class SingleCardBody extends Component{
                     <CardBody>
                         <CardText>
                             <ListGroup>
-                                <ListGroupItem color="success" style={{display : this.props.toggle.requester}}>Approver ID : {this.props.item.approver_id}</ListGroupItem>
-                                <ListGroupItem color="success" style={{display : this.props.toggle.approver}}>Requester ID : {this.props.item.requester_id}</ListGroupItem>
+                                <ListGroupItem color="info" style={{display : this.props.toggle.requester}}>Approver ID : {this.props.item.approver_id}</ListGroupItem>
+                                <ListGroupItem color="info" style={{display : this.props.toggle.approver}}>Requester ID : {this.props.item.requester_id}</ListGroupItem>
                                 <ListGroupItem color="info">Product ID : {this.props.item.product_id}</ListGroupItem>
-                                <ListGroupItem color="warning" style={{display : this.props.toggle.approver}}>Requester Comment : {this.props.item.requester_comment}</ListGroupItem>
-                                <ListGroupItem color="warning" style={{display : this.props.toggle.requester}}>Approver Comment : {this.props.item.approver_comment}</ListGroupItem>
-                                <ListGroupItem color="danger" style={{display : this.props.toggle.requester}}>Your Comment : {this.props.item.requester_comment}</ListGroupItem>
-                                <ListGroupItem color="danger" style={{display : this.props.toggle.approver}}>Your Comment : {this.props.item.approver_comment}</ListGroupItem>
+                                <ListGroupItem color="info" style={{display : this.props.toggle.approver}}>Requester Comment : {this.props.item.requester_comment}</ListGroupItem>
+                                <ListGroupItem color="info" style={{display : this.props.toggle.requester}}>Approver Comment : {this.props.item.approver_comment}</ListGroupItem>
+                                <ListGroupItem color="info" style={{display : this.props.toggle.requester}}>Your Comment : {this.props.item.requester_comment}</ListGroupItem>
+                                <ListGroupItem color="info" style={{display : this.props.toggle.approver}}>Your Comment : {this.props.item.approver_comment}</ListGroupItem>
                             </ListGroup>
                         </CardText>
-                        <Button onClick={() => {this.review(this.props.item)}} style={{display : this.props.toggle.approver}} color="primary">Review</Button>
+                        <Button onClick={() => {this.toggle(this.props.item)}} style={{display : this.props.toggle.approver}} color="primary">Review</Button>
                     </CardBody>
                 </Card>
+                <Modal isOpen={this.state.modal} toggle={this.state.toggle} backdrop="static" keyboard={true}>
+                    <ModalHeader toggle={this.state.toggle}>Request ID - #{this.props.item.request_id}</ModalHeader>
+                    <ModalBody>
+                        <Label for="approverNote">Approver Note (200 words)<span style={{color:'red'}}> *</span></Label>
+                        <Input type="textarea" name="approverNote" id="approverNote" rows="6"
+                            value={this.state.approverNote}
+                            onChange={this.handleInputChange} 
+                            onBlur = {this.handleBlur('approverNote')}
+                        />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="success" onClick={() => {this.toggle(this.props.item)}}>Approved</Button>{' '}
+                        <Button color="danger" onClick={() => {this.toggle(this.props.item)}}>Rejected</Button>{' '}
+                        <Button color="primary" onClick={() => {this.toggle(this.props.item)}}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
             </Col>
         );
     }
