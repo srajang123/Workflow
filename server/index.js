d5eacf5addcf5cfcff56c719e07cff732cb9dc5b
Mann Mehta <17it051@charusat.edu.in>
2020-09-02 06:43:31 +0530

    Serving production build of frontend folder

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname,'../frontend','build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
